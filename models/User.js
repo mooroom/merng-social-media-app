bddb4adff10beb49eeb7db189d551f87093c5005
mooroom <61869246+mooroom@users.noreply.github.com>
2021-02-14 16:01:34 +0900

    Register & Login Users

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const userSchema = new Schema({
   username: String,
   password: String,
   email: String,
-  createAt: String,
+  createdAt: String,
 });
 
 module.exports = model('User', userSchema);
